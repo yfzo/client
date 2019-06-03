162bca930d66bb16e6e63384ab2075f2875c1475
Yifei <yifeizhou.to@gmail.com>
2019-06-03 15:53:26 +0000

    wrote getAndPrintHTMLChunks function and completed step 1

---
+++ print-chunks.js
@@ -0,0 +1,20 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log(data, '\n');
+    });
+  });
+
+}
+
+getAndPrintHTMLChunks();
